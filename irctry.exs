b0920e35e0116867efd3a352204ae6e7e2ed5764
Ben Wendt <ben@theworkinggroup.ca>
2016-09-17 23:19:23 -0400

    Use binaries instead of char lists

--- irctry.exs
+++ irctry.exs
@@ -3,31 +3,30 @@ defmodule IRCTry do
   @initial_state %{socket: nil}
 
   def init(state, host, port, nick, channel, handler) do
-    opts = [:list, active: false]
-    {:ok, socket} = :gen_tcp.connect(host, port, opts)
+    {:ok, socket} = :gen_tcp.connect(host, port, [:binary, active: false])
     {:ok, %{state | socket: socket}}
-    say(socket, 'NICK #{nick}')
-    say(socket, 'USER ircbot 0 * #{nick}')
-    say(socket, 'JOIN ##{channel}')
-    say_to_chan(socket, channel, 'Hello')
+    say(socket, "NICK #{nick}")
+    say(socket, "USER ircbot 0 * #{nick}")
+    say(socket, "JOIN ##{channel}")
+    say_to_chan(socket, channel, "Hello")
     read(socket, channel, handler)
   end
 
   defp say(socket, msg) do
-    :gen_tcp.send(socket, msg ++ [10])
+    :gen_tcp.send(socket, msg <> "\n")
     IO.puts msg
   end
 
   defp say_to_chan(socket, channel, msg) do
-    say(socket, 'PRIVMSG ##{channel} :#{msg}')
+    say(socket, "PRIVMSG ##{channel} :#{msg}")
   end
 
   defp read(socket, channel, f) do
     {:ok, msg} = :gen_tcp.recv(socket, 0)
     IO.puts msg
     case msg do
-      'PING :' ++ host ->
-        say(socket, 'PONG :' ++ host)
+      "PING :" <> host ->
+        say(socket, "PONG :" <> host)
       _ ->
         reply = f.(msg)
         case reply do
@@ -44,7 +43,7 @@ end
 handler = fn (msg) ->
   smsg = String.Chars.to_string(msg)
   if String.contains?(smsg, "hi bot") do
-    {:ok, 'Hi back!'}
+    {:ok, "Hi back!"}
   end
 end
 IRCTry.init(%{socket: nil}, 'irc.freenode.net', 6667, 'IrcBot70101', 'fun_channel', handler)
